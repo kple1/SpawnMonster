d33f0a634aa9879575262fd117998275db644930
Leeple <kple1@naver.com>
2023-07-20 20:20:50 +0900

    권한 수정

--- src/main/java/io/spawnmonster/Command/Spawn.java
+++ src/main/java/io/spawnmonster/Command/Spawn.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import static io.spawnmonster.SpawnMonster.plugin;
 
 public class Spawn implements CommandExecutor {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player player) {
